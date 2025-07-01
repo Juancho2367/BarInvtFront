727c0447926c73b3a120678f9e5759b816d186dd
Juan David <juandavidq2367@gmail.com>
2025-07-01 17:48:23 -0500

    solucion error despliegue vercel

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   server: {
     port: 5173,
